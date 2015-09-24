5f8ca13947068dcc101c14273f7576787ca186de
Luv Agarwal <agarwal.iiit@gmail.com>
2015-09-24 19:31:22 +0530

    one function all command, global search (append /home/luv in search dirs), cleaning

--- test1.sh
+++ test1.sh
@@ -1,54 +1,57 @@
-function original_command(){
-c=$1
-location=$(whereis "$c")
-read -a loc <<< "$location"
-if [ -z "${loc[1]}" ]
-then
-  IFS=':' read -a loc <<< $loc
-  ret="builtin ${loc[0]}"
-  echo "$ret"
-else
-  echo "${loc[1]}"
-fi
+function path(){
+    c=$1
+    location=$(whereis "$c")
+    read -a loc <<< "$location"
+    if [ -z "${loc[1]}" ]
+    then
+        IFS=':' read -a loc <<< $loc
+        ret="builtin ${loc[0]}"
+        echo "$ret"
+    else
+        echo "${loc[1]}"
+    fi
 }
 
 function search_dir(){
-  find ~ -iname gitrepos
+    find ~ -iname gitrepos
+}
+
+function search_file(){
+    find ~ -name gitrepos
 }
 
 function dirs(){
-  echo "courses coding Downloads gitrepos notes semester"
+    echo "courses coding Downloads gitrepos notes semester"
 }
 
-function cd(){
-  d=$(dirs)
-  read -a directories <<< $d
-  #echo ${directories[0]}
-  original=$(original_command cd)
-  $original $@
-  if [ $? -ne 0 ]
-  then
-    #echo ${directories[@]}
-    res=$(find ${directories[@]} -not -path '*/\.*' -iname $@)
-    # echo "find ${directories[@]} -not -path '*/\.*' -iname $@"
-    # res has newlines but using $res will convert newlines to space
-    # while using "$res" keeps the newline as it is.
-    read -a result <<< $res
-    if [ -z "$result" ]
+function create_alias(){
+    alias cd="execute cd"
+}
+
+create_alias
+
+function execute(){
+    d=$(dirs)
+    read -a directories <<< $d
+    for i in `seq 0 $(expr ${#directories[@]} - 1)`
+    do
+        #echo ${directories[i]}
+        directories[i]=$HOME/${directories[i]}
+    done 
+    original=$(path $1)
+    err=$($original ${@:2} 2>&1)
+    if [ $? -ne 0 ]
     then
-      :
-    else
-      cd ${result[0]}
+        res=$(find ${directories[@]} -type d -not -path '*/\.*' -name ${@:2})
+        # echo "find ${directories[@]} -not -path '*/\.*' -iname $@"
+        # res has newlines but using $res will convert newlines to space
+        # while using "$res" keeps the newline as it is.
+        read -a result <<< $res
+        if [ -z "$result" ]
+        then
+            :
+        else
+            $1 ${result[0]}
+        fi
     fi
-  fi
-}
-
-function ls1(){
-original=$(original_command ls)
-$original $@
-  if [ $? -ne 0 ]
-  then
-    echo "meri script"
-    /bin/ls gitrepos
-  fi
 }
